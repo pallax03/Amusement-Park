18e1d89da75dd1d4b2cfc9d89f81e4e076aecfc9
Alex Mazzoni <alexmaz03@hotmail.it>
2024-05-30 15:58:09 +0200

    backend static folder

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
