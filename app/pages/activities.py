21594ca29956ce61bcf89ff31ead3848f3229af9
Alex Mazzoni <alexmaz03@hotmail.it>
2024-06-11 15:33:05 +0200

    added /entries to /visitors and init /activities, completed /activity/events

--- app/pages/activities.py
+++ app/pages/activities.py
@@ -1,57 +1,95 @@
 from flask import render_template, url_for, request, make_response, jsonify
 import json
 from datetime import datetime, timedelta
-from models import Activity, Category
+from models import Activity, Category, Limit, Constraint
 
 def activity(app, db):
     @app.route('/activities', methods=['GET'])
     def page_activities():
-        return render_template('activities.j2', categories=get_categories().json, 
-                                url_for_get_categories=url_for('get_categories'),
-                                url_for_get_activities=url_for('get_activities'),
+        return render_template('activities.j2',
                                 url_for_get_events=url_for('get_events'),
-                                url_for_add_activity=url_for('add_activity'))
+                                url_for_get_rides=url_for('get_rides'),
+                                url_for_get_categories=url_for('get_categories'),
+                                url_for_get_tariffs=url_for('get_tariffs'),
+                                url_for_get_limits=url_for('get_limits'))
 
 #APIs
-    # add an activity
-    @app.route('/api/activity', methods=['POST'])
-    def add_activity():
-        try:
-            data = request.get_json()
-            activity = Activity(
-                Nome=data['Nome'],
-                Descrizione=data['Descrizione'],
-                Categoria=data['Categoria'],
-                IsEvent=data['IsEvent']
-            )
-            db.session.add(activity)
-            db.session.commit()
-            return make_response(jsonify({'message': 'Attività creata'}), 201)
-        except Exception as e:
-            return make_response(jsonify({'error': str(e)}), 400)
 
-    @app.route('/api/activity/rides', methods=['GET'])
-    def get_activities():
-        return make_response(jsonify(Activity.query.filter_by(IsEvent=False).all()), 200)
-
-    @app.route('/api/activity/rides/categories', methods=['GET'])
-    def get_categories():
-        return make_response(jsonify(Category.query.all()), 200)
-    
-    @app.route('/api/activity/rides/categories', methods=['POST'])
-    def add_category():
-        try:
-            data = request.get_json()
-            category = Category(
-                Nome=data['Nome'],
-                Descrizione=data['Descrizione']
-            )
-            db.session.add(category)
-            db.session.commit()
-            return make_response(jsonify({'message': 'Categoria creata'}), 201)
-        except Exception as e:
-            return make_response(jsonify({'error': str(e)}), 400)
-
+    # get all the events
+    # /api/activity/events
     @app.route('/api/activity/events', methods=['GET'])
     def get_events():
-        return make_response(jsonify(Activity.query.filter_by(IsEvent=True).all()), 200)
+        try:
+            return make_response(jsonify(Activity.query.filter_by(IsEvent=True).all()), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 400)
+        
+    # add a new event
+    # /api/activity/events + json
+    @app.route('/api/activity/events', methods=['POST'])
+    def add_event():
+        try:
+            data = request.get_json()
+            new_event = Activity(Nome=data['Nome'], Descrizione=data['Descrizione'], Posti=data['Posti'], IsEvent=True)
+            db.session.add(new_event)
+            db.session.commit()
+            return make_response(jsonify({'message': f"evento {new_event.Nome} aggiunto"}), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 400)
+
+    # # delete a event
+    # # /api/activity/events/<int:id> + json
+    # @app.route('/api/activity/events/<int:id>', methods=['DELETE'])
+    # def delete_event(id):
+    #     try:
+    #         event = Activity.query.get(id)
+    #         db.session.delete(event)
+    #         db.session.commit()
+    #         return make_response(jsonify({'message': 'Event deleted'}), 200)
+    #     except Exception as e:
+    #         return make_response(jsonify({'error': str(e)}), 400)
+
+    # delete a event
+    # /api/activity/events + '?IdEvento=1'
+    @app.route('/api/activity/events', methods=['DELETE'])
+    def delete_event():
+        try:
+            event = Activity.query.filter_by(IdAttivita=int(request.args.get('IdEvento'))).first()
+            db.session.delete(event)
+            db.session.commit()
+            return make_response(jsonify({'message': f'evento {event.Nome} eliminato'}), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 400)
+
+    # get all the rides, can be filter by category and included tariff
+    # /api/activity/rides + '?IdCategoria=1&NomeTariffa=Giornaliero'
+    @app.route('/api/activity/rides', methods=['GET'])
+    def get_rides():
+        try:
+            activities = Activity.query.filter_by(IsEvent=False).all()
+
+            if request.args.get('IdCategoria') != None:
+                activities = [activity for activity in activities if activity.IdCategoria == int(request.args.get('IdCategoria'))]
+            if request.args.get('NomeTariffa') != None:
+                activities = [activity for activity in activities if request.args.get('NomeTariffa') in [include.Category.Nome for include in activity.tariffs]]
+            return make_response(jsonify(activities), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 400)
+
+    # get all the categories
+    # /api/activity/rides/categories
+    @app.route('/api/activity/rides/categories', methods=['GET']) 
+    def get_categories():
+        try:
+            return make_response(jsonify(Category.query.all()), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 400)
+        
+    # get all the limits
+    # /api/activity/rides/limits
+    @app.route('/api/activity/rides/limits', methods=['GET'])
+    def get_limits():
+        try:
+            return make_response(jsonify(Limit.query.all()), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': str(e)}), 400)
---
+++ app/static/js/activities.js
@@ -0,0 +1,182 @@
+
+// add tables empty row QoL
+
+
+function addEvent(id) {
+    fetch(url_for_get_events, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            Nome: document.getElementById(id+'Nome').value,
+            Descrizione: document.getElementById(id+'Descrizione').value,
+            Posti: document.getElementById(id+'Posti').value
+        })
+    })
+    .then(response => statusResponse(response));
+}
+
+function createOptionEvent(empty_row) {
+    let id = 'new-event_';
+    empty_row.innerHTML = '';
+    empty_row.className = '';
+
+    cell = empty_row.insertCell();
+    cell.innerHTML = 'nuovo evento';
+    cell = empty_row.insertCell();
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Nome';
+    input.maxLength = 50;
+    input.id = id+'Nome';
+    cell.appendChild(input);
+    cell = empty_row.insertCell();
+    input = document.createElement('input');
+    input.type = 'textarea';
+    input.placeholder = 'Descrizione';
+    input.maxLength = 200;
+    input.id = id+'Descrizione';
+    cell.appendChild(input);
+    cell = empty_row.insertCell();
+    input = document.createElement('input');
+    input.type = 'number';
+    input.placeholder = 'Posti Massimi';
+    input.oninput = function() { acceptInteger(this) };
+    input.id = id+'Posti';
+    cell.appendChild(input);
+    cell = empty_row.insertCell();
+    cell.innerHTML = '<button class="save" onclick="addEvent(\''+id+'\')">s</button>';
+}
+
+function deleteEvent(id) {
+    fetch(url_for_get_events + '?IdEvento=' + id, {
+        method: 'DELETE'
+    })
+    .then(response => statusResponse(response));
+}
+
+function getEvents() {
+    document.getElementById('table-body_events').innerHTML = '';
+    // addEmptyRow('#table-body_events', addEvent, 'event');
+    let empty_row = document.createElement('tr');
+    empty_row.className = 'empty_row';
+    let cell = document.createElement('td');
+    cell.colSpan = 5;
+    let button = document.createElement('button');
+    button.classList.add('add');
+    button.onclick = function() {createOptionEvent(empty_row)};
+    button.innerHTML = '+';
+    cell.appendChild(button);
+    empty_row.appendChild(cell);
+    document.getElementById('table-body_events').appendChild(empty_row);
+
+    fetch(url_for_get_events)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(event => {
+            let row = document.createElement('tr');
+            cell = document.createElement('td');
+            cell.innerHTML = '<button class="delete" onclick="deleteEvent('+event.IdAttivita+')">x</button>';
+            row.appendChild(cell);
+            cell = document.createElement('td');
+            cell.innerHTML = event.Nome;
+            row.appendChild(cell);
+            cell = document.createElement('td');
+            cell.innerHTML = event.Descrizione;
+            row.appendChild(cell);
+            cell = document.createElement('td');
+            cell.innerHTML = event.Posti;
+            row.appendChild(cell);
+            cell = document.createElement('td');
+            row.appendChild(cell);
+            document.getElementById('table-body_events').appendChild(row);
+        });
+    });
+}
+
+function getRides() {
+    let limit_id = document.getElementById('filter_limiti').value.split('_')[1] === undefined ? '' : document.getElementById('filter_limiti').value.split('_')[1];
+    let url = url_for_get_rides + '?';
+    url += 'category=' + document.getElementById('filter_categorie').value + '&';
+    url += 'limit=' + limit_id + '&';
+    url += 'tariff=' + document.getElementById('filter_tariffe').value;
+    
+    console.log(url);
+    
+    // fetch(url)
+    // .then(response => response.json())
+    // .then(data => {
+    //     document.getElementById('table-body_rides').innerHTML = '';
+    //     data.forEach(ride => {
+    //         document.getElementById('table_ride').innerHTML += '<tr>';
+    //         document.getElementById('table_ride').innerHTML += '<td>' + ride.Nome + '</td>';
+    //         document.getElementById('table_ride').innerHTML += '<td>' + ride.Categoria + '</td>';
+    //         document.getElementById('table_ride').innerHTML += '<td>' + ride.Limite + '</td>';
+    //         document.getElementById('table_ride').innerHTML += '<td>' + ride.Tariffa + '</td>';
+    //         document.getElementById('table_ride').innerHTML += '<td>' + ride.Descrizione + '</td>';
+    //         document.getElementById('table_ride').innerHTML += '</tr>';
+    //     });
+    // });
+}
+
+function filterRides() {
+    // FILTERS
+    
+    // CATEGORIES
+    document.getElementById('filter_categorie').innerHTML = '<option value="" selected>Tutte</option>';
+    fetch(url_for_get_categories)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(category => {
+            document.getElementById('filter_categorie').innerHTML += '<option value="' + category.Nome + '">' + category.Nome + '</option>';
+        });
+    });
+
+    // LIMITS
+    document.getElementById('filter_limiti').innerHTML = '<option value="" selected>Nessuno</option>';
+    fetch(url_for_get_limits)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(limit => {
+            document.getElementById('filter_limiti').innerHTML += '<option value="limit_'+limit.IdLimite+'">' + limit.Descrizione + '</option>';
+        });
+    });
+
+    // TARIFFS
+    document.getElementById('filter_tariffe').innerHTML = '<option value="" selected>Qualsiasi</option>';
+    fetch(url_for_get_tariffs)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(tariff => {
+            document.getElementById('filter_tariffe').innerHTML += '<option value="' + tariff.NomeTariffa + '">' + tariff.NomeTariffa + '</option>';
+        });
+    });
+
+    // AJAX
+    document.querySelectorAll('#filter_menu select').forEach((select) => {
+        select.addEventListener('change', function() {
+            getRides();
+        });
+    });
+}
+
+
+function Ajax() {
+    // change tables
+    document.querySelectorAll('.choices .options [name="activity"]').forEach((radio) => {
+        radio.addEventListener('click', function () {
+            document.querySelectorAll('.choices .options [name="activity"]').forEach((radio) => {
+                document.querySelector('#table_'+radio.id.split('_')[1]).parentElement.style.display = 'none';
+            });
+            document.querySelector('#table_'+radio.id.split('_')[1]).parentElement.style.display = '';
+        });
+    });
+
+    getEvents();
+
+    filterRides();
+    getRides();
+}
+
+Ajax();
